5e43e91041acc66d143467cc861d9ecab433d938
anph3 <112309078+anph3@users.noreply.github.com>
2023-07-27 11:19:55 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "test"}
+    return {"Hello": "testl"}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=80, log_level="info", access_log=True)
